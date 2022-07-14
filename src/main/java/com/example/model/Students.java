b46ae6e1bd881ddc66e0fe4306a484801d344d40
yangan <yangdingan_guizhou@163.com>
2022-07-14 16:28:31 +0800

    window测试一下

---
+++ src/main/java/com/example/model/Students.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import lombok.Data;
+
+/**
+ * @author ：yangan
+ * @date ：2022/7/14 下午3:29
+ * @description：
+ * @version:
+ */
+@Data
+public class Students {
+    private String stu_id;
+    private String stu_name;
+    private String companyName;
+    private String data;
+
+}
