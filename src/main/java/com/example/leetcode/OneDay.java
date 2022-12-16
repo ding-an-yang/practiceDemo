0804b08fca873c3a64bd77c6ed0a4b4b31ee4a4b
yangan <yangdingan_guizhou@163.com>
2022-12-16 10:39:52 +0800

    力扣算法，每日一题 第二题，寻找数组中心的索引。

--- src/main/java/com/example/leetcode/OneDay.java
+++ src/main/java/com/example/leetcode/OneDay.java
@@ -1,5 +1,7 @@
 package com.example.leetcode;
 
+import jdk.nashorn.internal.ir.Node;
+
 /**
  * @author ：yangan
  * @date ：2022/12/14 下午3:21
@@ -41,6 +43,54 @@ public class OneDay {
         }
         return result;
     }
+
+    /**
+     * 寻找数组中心的索引
+     * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+     * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+     * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+     * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+     * 示例 1：
+     * 输入：nums = [1, 7, 3, 6, 5, 6]
+     * 输出：3
+     * 解释：
+     * 中心下标是 3 。
+     * 左侧数之和 sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11 ，
+     * 右侧数之和 sum = nums[4] + nums[5] = 5 + 6 = 11 ，二者相等。
+     *
+     * 示例 2：
+     * 输入：nums = [1, 2, 3]
+     * 输出：-1
+     * 解释：
+     * 数组中不存在满足此条件的中心下标。
+     *
+     *  示例 3：
+     * 输入：nums = [2, 1, -1]
+     * 输出：0
+     * 解释：
+     * 中心下标是 0 。
+     * 左侧数之和 sum = 0 ，（下标 0 左侧不存在元素），
+     * 右侧数之和 sum = nums[1] + nums[2] = 1 + -1 = 0 。
+     * @param nums
+     * @return
+     */
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        int leftSum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        for (int j = 0;j < nums.length;j++){
+            sum -= nums[j];
+            if (sum == leftSum){
+                return j;
+            }
+            leftSum += nums[j];
+        }
+        return -1;
+    }
+
+
 
 
 }
