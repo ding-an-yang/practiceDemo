8eb0ec94e990d52f3a81ab358593394bdd7a2b4b
yangan <yangdingan_guizhou@163.com>
2023-07-21 17:18:29 +0800

    日常练习提交

---
+++ src/main/java/com/example/dto/DriveExamQuestionsDTO.java
@@ -0,0 +1,41 @@
+package com.example.dto;
+
+/**
+ * @author ：yangan
+ * @date ：2023/7/21 上午10:16
+ * @description：
+ * @version:
+ */
+public class DriveExamQuestionsDTO {
+    private Long id;
+    private Long questionId;
+    private Short mediaType;
+//    private Short type;
+//    private Short subject;
+    private Long chapterId;
+    private String label;
+    private Integer mediaId;
+    private Integer mediaWidth;
+    private Integer mediaHeight;
+    private Integer answer;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String optionE;
+    private String optionF;
+    private String optionG;
+    private String optionH;
+    private Integer difficulty;
+    private String wrongRate;
+    private Long optionType;
+    private String question;
+    private String explain;
+    private String keywords;
+    private String media;
+//    private Integer mId;
+//    private String mMediaContent;
+    private String mUrl;
+    private String chapter;
+    private Integer answers;
+}
---
+++ src/main/java/com/example/util/JSONUtil.java
@@ -0,0 +1,54 @@
+package com.example.util;
+
+import com.alibaba.fastjson.JSON;
+import com.example.dto.DriveExamQuestionsDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import springfox.documentation.spring.web.json.Json;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * @author ：yangan
+ * @date ：2023/7/21 上午9:42
+ * @description：
+ * @version:
+ */
+public class JSONUtil {
+    static void readWithClassLoader() throws IOException {
+        String fileName = "our-20230720.json";
+        ClassLoader  classLoader =  JSONUtil.class.getClassLoader();
+        BufferedReader reader = null;
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder content = new StringBuilder();
+        String line = reader.readLine();
+        int i = 0;
+        while (line != null) {//line != null
+            if (line.contains("OptionE:")){
+                System.out.println(line.substring(8));
+            }
+            content.append(line);
+            line = reader.readLine();
+            i++;
+        }
+        System.out.println(i);
+//        return content.toString();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        //String s = StringEscapeUtils.unescapeJava(readWithClassLoader());
+//        DriveExamQuestionsDTO driveExamQuestionsDTO = JSON.parseObject(readWithClassLoader(), DriveExamQuestionsDTO.class);
+//        System.out.println(driveExamQuestionsDTO);
+        readWithClassLoader();
+//        System.out.println(readWithClassLoader());
+    }
+
+}
